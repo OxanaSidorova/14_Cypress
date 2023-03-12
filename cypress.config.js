4054b60280f6d0a73f1204492e989454f96d0a38
OxanaSidorova <106552165+OxanaSidorova@users.noreply.github.com>
2023-03-12 13:05:10 +0000

    15.1 dashboard added

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,17 @@
 const { defineConfig } = require("cypress");
 
+// module.exports = {
+//   projectId: "1j7ygf",
+//   // The rest of the Cypress config options go here...
+// }
+
+
 module.exports = defineConfig({
+  projectId: '1j7ygf', 
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-});
+})
+
---
+++ cypress/e2e/test.js
@@ -0,0 +1,5 @@
+describe ("first test suit",()=> {
+    it("first test", ()=> {
+    cypress.visit("'https://staging.lpitko.ru/")
+     })
+} )
