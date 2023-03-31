b79ea95440f3c15eebfdbefac60305cb20ba38e9
OxanaSidorova <106552165+OxanaSidorova@users.noreply.github.com>
2023-03-31 19:11:34 +0100

    Page object added

--- cypress/e2e/16.1.registration.cy.js
+++ cypress/e2e/16.1.registration.cy.js
@@ -1,34 +1,40 @@
-describe ("registration",()=> {
-    const selectorName= (':nth-child(3) > .frm')
-    beforeEach(() => {
-      cy.visit("/register")
-    })
-    it("name and email letters with correct email", ()=> {
-    
-       cy.userName(selectorName,'Mash'),
-       cy.get('[name="email"]').type('mash@mail.ru'),
-       cy.get('.btn-main').click({force:true}), 
-       cy.request("/api/login?redirect=%2F")
-      cy.get("[class='picture-notice__title txt-h3--semi txt']").should ("have.text", "Письмо отправлено!")
-     })
+describe("registration", () => {
+  const selectorName = ":nth-child(3) > .frm";
+  beforeEach(() => {
+    cy.visit("/register");
+  });
+  it("name and email letters with correct email", () => {
+    cy.typeUserName(selectorName, "Mash"),
+      cy.get('[name="email"]').type("mash@mail.ru"),
+      cy.get(".btn-main").click({ force: true }),
+      cy.request("/api/login?redirect=%2F");
+    cy.get("[class='picture-notice__title txt-h3--semi txt']").should(
+      "have.text",
+      "Письмо отправлено!"
+    );
+  })
 
-     it("name with numbers with correct email", ()=> {
-    
-       cy.userName(selectorName,'123456'),
+  it("name with numbers with correct email", () => {
+    cy.typeUserName(selectorName, "123456"),
       //  cy.get("[name='email']").type('oxana.s.sidorova@gmail.com'),
-       cy.get('[name="email"]').type('masha@mail.ru'),
-       cy.get('.btn-main').click(), 
-       cy.request("/api/login?redirect=%2F")
-      cy.get("[class='picture-notice__title txt-h3--semi txt']").should ("have.text", "Письмо отправлено!")
-     })
+      cy.get('[name="email"]').type("masha@mail.ru"),
+      cy.get(".btn-main").click(),
+      cy.request("/api/login?redirect=%2F");
+    cy.get("[class='picture-notice__title txt-h3--semi txt']").should(
+      "have.text",
+      "Письмо отправлено!"
+    );
+  });
 
-     it("name wirh special cymbols and correct email", ()=> {
-    
-        cy.userName(selectorName,'###Masha'),
-       //  cy.get("[name='email']").type('oxana.s.sidorova@gmail.com'),
-        cy.get('[name="email"]').type('masha@mail.ru'),
-        cy.get('.btn-main').click(), 
-        cy.request("/api/login?redirect=%2F")
-       cy.get("[class='picture-notice__title txt-h3--semi txt']").should ("have.text", "Письмо отправлено!")
-      })
-})
+  it("name wirh special cymbols and correct email", () => {
+    cy.typeUserName(selectorName, "###Masha"),
+      //  cy.get("[name='email']").type('oxana.s.sidorova@gmail.com'),
+      cy.get('[name="email"]').type("masha@mail.ru"),
+      cy.get(".btn-main").click(),
+      cy.request("/api/login?redirect=%2F");
+    cy.get("[class='picture-notice__title txt-h3--semi txt']").should(
+      "have.text",
+      "Письмо отправлено!"
+    );
+  });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,14 +23,27 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+// const cypress = require("cypress");
+
  
 Cypress.Commands.add('enterLogin',(selector, email) => {
     cy.get(selector).type(email);
   });
-  Cypress.Commands.add('userName',(selector, name) => {
+  Cypress.Commands.add('typeUserName',(selector, name) => {
     cy.get(selector).type(name);
   });
 
-  Cypress.Commands.add('userEmail',(selector, email) => {
+  Cypress.Commands.add('typeUserEmail',(selector, email) => {
     cy.get(selector).type(email);
 });
+
+Cypress.Commands.add("valigLogin")
+
+
+Cypress.Commands.add('changePassword',(typeUserName, newPassword)=>{
+    cy.contains(typeUserName).click({force: true});
+    cy.get(".layout-column-start > :nth-child(1) > .frm").type(newPassword);
+    cy.get(':nth-child(4) > .form-page-group__main > .layout-column-start > :nth-child(2) > .frm').type(newPassword);
+    cy.get('.layout-row-end > .btn-service').click()
+    })
